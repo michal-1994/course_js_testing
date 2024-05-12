42e16db118c6be6db157048576a04dff65878fb7
Michal <michalgrzegorczyk04@gmail.com>
2024-05-12 14:02:12 +0200

    test with errors

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -45,3 +45,11 @@ it("should yield 0 if an empty array is provided", () => {
 
   expect(result).toBe(0);
 });
+
+it("should throw an error if no value is passed into the function", () => {
+  const resultFn = () => {
+    add();
+  };
+
+  expect(resultFn).toThrow();
+});
