d9c0635939faf28c4bb0c16a7f3972e0a80079e8
Michal <michalgrzegorczyk04@gmail.com>
2024-05-12 15:56:13 +0200

    add integration test

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,31 +1,43 @@
 /** @format */
 
-import { expect, it } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, expect, it } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-it("should transform a string number to a number of type number", () => {
-  const input = "1";
+describe("transformToNumber()", () => {
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
 
-  const result = transformToNumber(input);
+    const result = transformToNumber(input);
 
-  expect(result).toBeTypeOf("number");
+    expect(result).toBeTypeOf("number");
+  });
+
+  it("should transform a string number to a number of type number", () => {
+    const input = "1";
+
+    const result = transformToNumber(input);
+
+    expect(result).toBe(+input);
+  });
+
+  it("should yield NaN for non-transformable values", () => {
+    const input = "invalid";
+    const input2 = {};
+
+    const result = transformToNumber(input);
+    const result2 = transformToNumber(input2);
+
+    expect(result).toBeNaN();
+    expect(result2).toBeNaN();
+  });
 });
 
-it("should transform a string number to a number of type number", () => {
-  const input = "1";
+describe("cleanNumbers()", () => {
+  it("should return an array of number values if an array of string number values is provided", () => {
+    const numberValues = ["1", "2", "3", "4", "5"];
 
-  const result = transformToNumber(input);
+    const cleanedNumbers = cleanNumbers(numberValues);
 
-  expect(result).toBe(+input);
-});
-
-it("should yield NaN for non-transformable values", () => {
-  const input = "invalid";
-  const input2 = {};
-
-  const result = transformToNumber(input);
-  const result2 = transformToNumber(input2);
-
-  expect(result).toBeNaN();
-  expect(result2).toBeNaN();
+    expect(cleanedNumbers[0]).toBeTypeOf("number");
+  });
 });
