6c21f63156a58c25432b145a09cefb5985a627ae
Michal <michalgrzegorczyk04@gmail.com>
2024-05-12 13:42:44 +0200

    use AAA pattern

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -4,6 +4,16 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should sumarize all number values in an array", () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange
+  const numbers = [1, 2, 3, 4, 5];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce(
+    (prevValue, curValue) => prevValue + curValue,
+    0
+  );
+  expect(result).toBe(expectedResult);
 });
