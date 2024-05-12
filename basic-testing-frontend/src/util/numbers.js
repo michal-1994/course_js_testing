a0de87758e2fffc2078b87fb9e4618aa585eb58d
Michal <michalgrzegorczyk04@gmail.com>
2024-05-12 15:31:01 +0200

    refactor code

--- basic-testing-frontend/src/util/numbers.js
+++ basic-testing-frontend/src/util/numbers.js
@@ -1,5 +1,20 @@
 /** @format */
 
+import { validateNumber, validateStringNotEmpty } from "./validation.js";
+
 export function transformToNumber(value) {
   return +value;
+}
+
+export function cleanNumbers(numberValues) {
+  const numbers = [];
+
+  for (const numberInput of numberValues) {
+    validateStringNotEmpty(numberInput);
+    const number = transformToNumber(numberInput);
+    validateNumber(number);
+    numbers.push(number);
+  }
+
+  return numbers;
 }
