d73564abad64a1c5ebbed0e3f1393cc37e4634fc
Michal <michalgrzegorczyk04@gmail.com>
2024-05-12 14:22:14 +0200

    add more tests

---
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import { expect, it } from "vitest";
+import { transformToNumber } from "./numbers";
+
+it("should transform a string number to a number of type number", () => {
+  const input = "1";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeTypeOf("number");
+});
+
+it("should yield NaN for non-transformable values", () => {
+  const input = "invalid";
+
+  const result = transformToNumber(input);
+
+  expect(result).toBeNaN();
+});
