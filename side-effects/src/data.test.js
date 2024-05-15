6f1f00079fc853558d68a553555ac778ed82bcd9
Michal <michalgrzegorczyk04@gmail.com>
2024-05-15 19:08:57 +0200

    work with spies

---
+++ side-effects/src/data.test.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import { describe, expect, it, vi } from "vitest";
+import { generateReportData } from "./data";
+
+describe("generateReportData()", () => {
+  it("should execute logFn if provided", () => {
+    const logger = vi.fn();
+
+    generateReportData(logger);
+
+    expect(logger).toBeCalled();
+  });
+});
