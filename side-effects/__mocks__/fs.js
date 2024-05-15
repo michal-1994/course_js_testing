5bda45153feb33856f4ca5ae16bd6eae154f42c1
Michal <michalgrzegorczyk04@gmail.com>
2024-05-15 19:27:29 +0200

    create custom mock

---
+++ side-effects/__mocks__/fs.js
@@ -0,0 +1,11 @@
+/** @format */
+
+import { vi } from "vitest";
+
+export const promises = {
+  writeFile: vi.fn((path, data) => {
+    return new Promise((resolve, reject) => {
+      resolve();
+    });
+  }),
+};
