b6116d58a510d654225b54e52d20fbec8b57f19c
Michal <michalgrzegorczyk04@gmail.com>
2024-05-18 04:43:04 +0200

    use mocing logic

--- more-mocks/util/http.js
+++ more-mocks/util/http.js
@@ -1,10 +1,12 @@
-import {HttpError} from './errors.js';
+/** @format */
+
+import { HttpError } from "./errors.js";
 
 export async function sendDataRequest(data) {
-  const response = await fetch('https://dummy-site.dev/posts', {
-    method: 'POST',
+  const response = await fetch("https://dummy-site.dev/posts", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
@@ -12,7 +14,11 @@ export async function sendDataRequest(data) {
   const responseData = await response.json();
 
   if (!response.ok) {
-    throw new HttpError(response.status, 'Sending the request failed.', responseData);
+    throw new HttpError(
+      response.status,
+      "Sending the request failed.",
+      responseData
+    );
   }
 
   return responseData;
--- more-mocks/util/http.test.js
+++ more-mocks/util/http.test.js
@@ -6,6 +6,9 @@ import { sendDataRequest } from "./http";
 const testResponseData = { testKey: "testData" };
 const testFetch = vi.fn((url, options) => {
   return new Promise((resolve, reject) => {
+    if (typeof options.body !== "string") {
+      return reject("Not a string");
+    }
     const testResponse = {
       ok: true,
       json() {
@@ -24,4 +27,18 @@ it("should return any available response data", () => {
   const testData = { key: "test" };
 
   return expect(sendDataRequest(testData)).resolves.toEqual(testResponseData);
+});
+
+it("should convert the provided data to JSON before sending the request", async () => {
+  const testData = { key: "test" };
+
+  let errorMessage;
+
+  try {
+    await sendDataRequest(testData);
+  } catch (error) {
+    errorMessage = error;
+  }
+
+  expect().not.toBe("Not a string");
 });
