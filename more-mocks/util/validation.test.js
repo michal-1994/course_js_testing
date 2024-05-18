ac53401a7bcb5a5ee81b5dbd81c93c0af02f5a7a
Michal <michalgrzegorczyk04@gmail.com>
2024-05-18 04:26:10 +0200

    add validation tests

---
+++ more-mocks/util/validation.test.js
@@ -0,0 +1,29 @@
+/** @format */
+
+import { expect, it } from "vitest";
+import { validateNotEmpty } from "./validation";
+
+it("should throw an error if an empty string is provided as a value", () => {
+  const testInput = "";
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).throw();
+});
+
+it("should throw an error if an empty string is provided as a value", () => {
+  const testInput = "   ";
+
+  const validationFn = () => validateNotEmpty(testInput);
+
+  expect(validationFn).throw();
+});
+
+it("should throw an error with the provided error message", () => {
+  const testInput = "";
+  const testErrorMessage = "Test";
+
+  const validationFn = () => validateNotEmpty(testInput, testErrorMessage);
+
+  expect(validationFn).throw(testErrorMessage);
+});
